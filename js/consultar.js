0f8fd02394b91440d489d395afdc9e7a64940538
Vitor Augusto Batista Rocha <44412471+Vitorrrocha@users.noreply.github.com>
2019-08-01 02:43:10 -0300

    Update consultar.js

--- js/consultar.js
+++ js/consultar.js
@@ -18,198 +18,228 @@ function alteraTable(estadoFormatado,) {
      var reg = window.document.querySelector("td.reg")
 
      
-     if (estadoFormatado == "sao paulo") { //1
-          sigla.innerHTML = "SP"
-          nomeEx.innerHTML = "São Paulo"
-          cap.innerHTML = "São Paulo"
-          reg.innerHTML = "Sudeste"
-          document.getElementById('alerta').style.display = 'none'   // desabilita a imagem de erro
-     }
-     else if (estadoFormatado == "santa catarina") { //2
-          sigla.innerHTML = "SC"
-          nomeEx.innerHTML = "Santa Catarina"
-          cap.innerHTML = "Florianópolis"
-          reg.innerHTML = "Sul"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "rio de janeiro") { //3
-          sigla.innerHTML = "RJ"
-          nomeEx.innerHTML = "Rio de Janeiro"
-          cap.innerHTML = "Rio de Janeiro"
-          reg.innerHTML = "Sudeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "acre") { //4
+     switch (estadoFormatado) {
           
-          sigla.innerHTML = "AC"
-          nomeEx.innerHTML = "Acre"
-          cap.innerHTML = "Rio Branco"
-          reg.innerHTML = "Norte"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "rio grande do norte") {//5
-          sigla.innerHTML = "RN"
-          nomeEx.innerHTML = "Rio Grande do Norte"
-          cap.innerHTML = "Natal"
-          reg.innerHTML = "Nordeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "rio grande do sul") {//6
-          sigla.innerHTML = "RS"
-          nomeEx.innerHTML = "Rio Grande do Sul"
-          cap.innerHTML = "Porto Alegre"
-          reg.innerHTML = "Sul"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "pernambuco") {//7
-          sigla.innerHTML = "PE"
-          nomeEx.innerHTML = "Pernambuco"
-          cap.innerHTML = "Recife"
-          reg.innerHTML = "Nordeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "piaui") {//8
-          sigla.innerHTML = "PI"         
-          nomeEx.innerHTML = "Piauí"         
-          cap.innerHTML = "Teresina"         
-          reg.innerHTML = "Nordeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "rondonia") {//9
-          sigla.innerHTML = "RO"         
-          nomeEx.innerHTML = "Rondônia"         
-          cap.innerHTML = "Porto Velho"         
-          reg.innerHTML = "Norte"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "roraima") {//10
-          sigla.innerHTML = "RR"         
-          nomeEx.innerHTML = "Roraima"         
-          cap.innerHTML = "Boa Vista"         
-          reg.innerHTML = "Norte"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "sergipe") {//11
-          sigla.innerHTML = "SE"         
-          nomeEx.innerHTML = "Sergipe"         
-          cap.innerHTML = "Aracaju"         
-          reg.innerHTML = "Nordeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "tocantins") {//12
-          sigla.innerHTML = "TO"         
-          nomeEx.innerHTML = "Tocantins"         
-          cap.innerHTML = "palmas"         
-          reg.innerHTML = "Norte"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "parana") {//13
-          sigla.innerHTML = "PR"         
-          nomeEx.innerHTML = "Paraná"         
-          cap.innerHTML = "Curitiba"         
-          reg.innerHTML = "Sul"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "paraiba") {//14
-          sigla.innerHTML = "PB"         
-          nomeEx.innerHTML = "Paraíba"         
-          cap.innerHTML = "João Pessoa"         
-          reg.innerHTML = "Nordeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "para") {//15
-          sigla.innerHTML = "PA"         
-          nomeEx.innerHTML = "Pará"         
-          cap.innerHTML = "Belém"         
-          reg.innerHTML = "Nordeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "minas gerais") {//16
-          sigla.innerHTML = "MG"         
-          nomeEx.innerHTML = "Minas Gerais"         
-          cap.innerHTML = "Belo Horizonte"         
-          reg.innerHTML = "Sudeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "mato groso do sul") {//17
-          sigla.innerHTML = "MS"         
-          nomeEx.innerHTML = "Mato Grosso do Sul"         
-          cap.innerHTML = "Campo Grande"         
-          reg.innerHTML = "Centro-Oeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "mato grosso") {//18
-          sigla.innerHTML = "MT"         
-          nomeEx.innerHTML = "Mato Grosso"         
-          cap.innerHTML = "Cuiabá"         
-          reg.innerHTML = "Centro-Oeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "maranhao") {//19
-          sigla.innerHTML = "MA"         
-          nomeEx.innerHTML = "Maranhão"         
-          cap.innerHTML = "São Luís"         
-          reg.innerHTML = "nordeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "goias") {//20
-          sigla.innerHTML = "GO"         
-          nomeEx.innerHTML = "Goiás"         
-          cap.innerHTML = "Goiânia"         
-          reg.innerHTML = "Centro-Oeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "espirito santo") {//21
-          sigla.innerHTML = "ES"         
-          nomeEx.innerHTML = "Espírito Santo"         
-          cap.innerHTML = "Vitória"         
-          reg.innerHTML = "Sudeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "ceara") {//22
-          sigla.innerHTML = "CE"         
-          nomeEx.innerHTML = "Ceará"         
-          cap.innerHTML = "Fortaleza"         
-          reg.innerHTML = "Nordeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "bahia") {//23
-          sigla.innerHTML = "BA"         
-          nomeEx.innerHTML = "Bahia"         
-          cap.innerHTML = "Salvador"         
-          reg.innerHTML = "Nordeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado== "amazonas") {//24
-          sigla.innerHTML = "AM"         
-          nomeEx.innerHTML = "Amazonas"         
-          cap.innerHTML = "Manaus"         
-          reg.innerHTML = "Norte"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "amapa") {//25
-          sigla.innerHTML = "AP"         
-          nomeEx.innerHTML = "Amapá"         
-          cap.innerHTML = "Macapá"         
-          reg.innerHTML = "Norte"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "alagoas") {//26
-          sigla.innerHTML = "AL"         
-          nomeEx.innerHTML = "Alagoas"         
-          cap.innerHTML = "Maceió"         
-          reg.innerHTML = "Nordeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else if (estadoFormatado == "distrito federal") {//27
-          sigla.innerHTML = "DF"
-          nomeEx.innerHTML = "Distrito Federal"
-          cap.innerHTML = "Brasilia"
-          reg.innerHTML = "Centro-Oeste"
-          document.getElementById('alerta').style.display = 'none'
-     }
-     else{
-          document.getElementById('alerta').style.display = 'block';    //mostra a mensagem na tela
+          case "sao paulo" ://1
+               sigla.innerHTML = "SP"
+               nomeEx.innerHTML = "São Paulo"
+               cap.innerHTML = "São Paulo"
+               reg.innerHTML = "Sudeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break   // desabilita a imagem de erro
+          
+          case "santa catarina"://2
+               sigla.innerHTML = "SC"
+               nomeEx.innerHTML = "Santa Catarina"
+               cap.innerHTML = "Florianópolis"
+               reg.innerHTML = "Sul"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "rio de janeiro"://3
+               sigla.innerHTML = "RJ"
+               nomeEx.innerHTML = "Rio de Janeiro"
+               cap.innerHTML = "Rio de Janeiro"
+               reg.innerHTML = "Sudeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "acre" ://4
+               
+               sigla.innerHTML = "AC"
+               nomeEx.innerHTML = "Acre"
+               cap.innerHTML = "Rio Branco"
+               reg.innerHTML = "Norte"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "rio grande do norte": //5
+               sigla.innerHTML = "RN"
+               nomeEx.innerHTML = "Rio Grande do Norte"
+               cap.innerHTML = "Natal"
+               reg.innerHTML = "Nordeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "rio grande do sul": //6
+               sigla.innerHTML = "RS"
+               nomeEx.innerHTML = "Rio Grande do Sul"
+               cap.innerHTML = "Porto Alegre"
+               reg.innerHTML = "Sul"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "pernambuco": //7
+               sigla.innerHTML = "PE"
+               nomeEx.innerHTML = "Pernambuco"
+               cap.innerHTML = "Recife"
+               reg.innerHTML = "Nordeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "piaui": //8
+               sigla.innerHTML = "PI"         
+               nomeEx.innerHTML = "Piauí"         
+               cap.innerHTML = "Teresina"         
+               reg.innerHTML = "Nordeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "rondonia": //9
+               sigla.innerHTML = "RO"         
+               nomeEx.innerHTML = "Rondônia"         
+               cap.innerHTML = "Porto Velho"         
+               reg.innerHTML = "Norte"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "roraima": //10
+               sigla.innerHTML = "RR"         
+               nomeEx.innerHTML = "Roraima"         
+               cap.innerHTML = "Boa Vista"         
+               reg.innerHTML = "Norte"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "sergipe": //11
+               sigla.innerHTML = "SE"         
+               nomeEx.innerHTML = "Sergipe"         
+               cap.innerHTML = "Aracaju"         
+               reg.innerHTML = "Nordeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "tocantins": //12
+               sigla.innerHTML = "TO"         
+               nomeEx.innerHTML = "Tocantins"         
+               cap.innerHTML = "palmas"         
+               reg.innerHTML = "Norte"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "parana": //13
+               sigla.innerHTML = "PR"         
+               nomeEx.innerHTML = "Paraná"         
+               cap.innerHTML = "Curitiba"         
+               reg.innerHTML = "Sul"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "paraiba": //14
+               sigla.innerHTML = "PB"         
+               nomeEx.innerHTML = "Paraíba"         
+               cap.innerHTML = "João Pessoa"         
+               reg.innerHTML = "Nordeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "para": //15
+               sigla.innerHTML = "PA"         
+               nomeEx.innerHTML = "Pará"         
+               cap.innerHTML = "Belém"         
+               reg.innerHTML = "Nordeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "minas gerais": //16
+               sigla.innerHTML = "MG"         
+               nomeEx.innerHTML = "Minas Gerais"         
+               cap.innerHTML = "Belo Horizonte"         
+               reg.innerHTML = "Sudeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "mato groso do sul": //17
+               sigla.innerHTML = "MS"         
+               nomeEx.innerHTML = "Mato Grosso do Sul"         
+               cap.innerHTML = "Campo Grande"         
+               reg.innerHTML = "Centro-Oeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "mato grosso": //18
+               sigla.innerHTML = "MT"         
+               nomeEx.innerHTML = "Mato Grosso"         
+               cap.innerHTML = "Cuiabá"         
+               reg.innerHTML = "Centro-Oeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "maranhao": //19
+               sigla.innerHTML = "MA"         
+               nomeEx.innerHTML = "Maranhão"         
+               cap.innerHTML = "São Luís"         
+               reg.innerHTML = "nordeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "goias": //20
+               sigla.innerHTML = "GO"         
+               nomeEx.innerHTML = "Goiás"         
+               cap.innerHTML = "Goiânia"         
+               reg.innerHTML = "Centro-Oeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "espirito santo": //21
+               sigla.innerHTML = "ES"         
+               nomeEx.innerHTML = "Espírito Santo"         
+               cap.innerHTML = "Vitória"         
+               reg.innerHTML = "Sudeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "ceara": //22
+               sigla.innerHTML = "CE"         
+               nomeEx.innerHTML = "Ceará"         
+               cap.innerHTML = "Fortaleza"         
+               reg.innerHTML = "Nordeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "bahia": //23
+               sigla.innerHTML = "BA"         
+               nomeEx.innerHTML = "Bahia"         
+               cap.innerHTML = "Salvador"         
+               reg.innerHTML = "Nordeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "amazonas": //24
+               sigla.innerHTML = "AM"         
+               nomeEx.innerHTML = "Amazonas"         
+               cap.innerHTML = "Manaus"         
+               reg.innerHTML = "Norte"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "amapa": //25
+               sigla.innerHTML = "AP"         
+               nomeEx.innerHTML = "Amapá"         
+               cap.innerHTML = "Macapá"         
+               reg.innerHTML = "Norte"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "alagoas": //26
+               sigla.innerHTML = "AL"         
+               nomeEx.innerHTML = "Alagoas"         
+               cap.innerHTML = "Maceió"         
+               reg.innerHTML = "Nordeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+          
+          case "distrito federal": //27
+               sigla.innerHTML = "DF"
+               nomeEx.innerHTML = "Distrito Federal"
+               cap.innerHTML = "Brasilia"
+               reg.innerHTML = "Centro-Oeste"
+               document.getElementById('alerta').style.display = 'none' 
+               break
+
+          default: 
+               document.getElementById('alerta').style.display = 'block';
+               break
      }
 }
 
